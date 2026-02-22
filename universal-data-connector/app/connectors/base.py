d5ffc4f3bbf83a04616cccc602ac669c3919ec78
Abdul Ahad Rauf <120673653+AbdulAhadRauf@users.noreply.github.com>
2026-02-22 22:56:28 +0530

    Delete universal-data-connector directory

--- universal-data-connector/app/connectors/base.py
+++
@@ -1,9 +0,0 @@
-
-from abc import ABC, abstractmethod
-from typing import List, Dict, Any
-
-class BaseConnector(ABC):
-
-    @abstractmethod
-    def fetch(self, **kwargs) -> List[Dict[str, Any]]:
-        pass
